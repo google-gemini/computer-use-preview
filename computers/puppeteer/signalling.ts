764b7472529f1e576b0efed841d1a4ba3f5beb3a
Wenlei (Frank) He <wlhe@google.com>
2025-05-07 19:20:06 +0000

    resvert signalling.ts

--- computers/puppeteer/signalling.ts
+++ computers/puppeteer/signalling.ts
@@ -1,20 +1,28 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import http from 'http';
 import {PubSub, Message, Subscription} from '@google-cloud/pubsub';
-import { createClient } from 'redis';
-import {Keepaliver} from './keepaliver';
-
-const crypto = require("crypto");
-type RedisClient = ReturnType<typeof createClient>;
 
 export interface MessagingChannel {
-  subscribe(handler: (message: CommandMessage) => Promise<void>): Promise<void>;
+  subscribe(handler: (message: CommandMessage) => Promise<void>): void;
   disconnect(): Promise<void>;
 }
 
 export interface CommandMessage {
   id: string;
   command: string|Record<string, any>;
-  publishScreenshot(screenshot: string, sessionID: string): Promise<void>;
+  publishScreenshot(screenshot: string, sessionID: string, url: string): Promise<void>;
   publishError(error: string): Promise<void>;
 }
 
@@ -28,9 +36,9 @@ class HttpMessage implements CommandMessage {
     this.command = command;
     this.res = res;
   }
-  async publishScreenshot(screenshot: string, sessionID: string): Promise<void> {
+  async publishScreenshot(screenshot: string, sessionID: string, url: string): Promise<void> {
     this.res.writeHead(200, { 'Content-Type': 'application/json' });
-    this.res.end(JSON.stringify({ id: this.id, session_id: sessionID, screenshot }));
+    this.res.end(JSON.stringify({ id: this.id, session_id: sessionID, screenshot, url }));
   }
 
   async publishError(error: string): Promise<void> {
@@ -42,58 +50,23 @@ class HttpMessage implements CommandMessage {
 export class HttpChannel implements MessagingChannel {
   port: number;
   server?: http.Server;
-  instanceId: string;  // Unique ID for this instance
-
-  private keepaliver?: Keepaliver;  // For keeping this instance alive.
 
   constructor(port: number) {
     this.port = port;
-    this.instanceId = crypto.randomUUID();
-    console.log(`Instance ID: ${this.instanceId}`);
   }
 
-  async subscribe(handler: (message: CommandMessage) => Promise<void>) {
+  subscribe(handler: (message: CommandMessage) => Promise<void>) {
     this.server = http.createServer(async (req, res) => {
-      console.log(`Received HTTP request: ${req.method} ${req.url}`);
-       if (req.url === '/instance_id') {
-	 await this.keepaliver?.handleInstanceIdRequest(req, res);
-       }
-       else if (req.url === '/keepalive') {
-         await this.keepaliver?.handleKeepaliveRequest(req, res);
-       }
-
-       else {
-	 try {
-           const body = await this.getBody(req);
-           console.log("HTTP Body:", body);
-           const json = JSON.parse(body);
-           // Adjust based on your CommandMessage structure
-           const command = "command" in json ? json.command : json;
-           await handler(new HttpMessage(command, res));
-         } catch (error) {
-           console.error("Error handling HTTP request:", error);
-           if (!res.headersSent) {
-             res.writeHead(500, { 'Content-Type': 'application/json' });
-             res.end(JSON.stringify({ error: 'Internal Server Error' }));
-           }
-         }
-
-       }
-    })
-
-    // Create the keepaliver
-    this.keepaliver = new Keepaliver(this.server, this.instanceId); // Updated class and property name
-
-    // Start the HTTP server listening
-    this.server.listen(this.port, () => {
-      console.log(`HTTP server listening on port ${this.port}`);
-    });
+      const body = await this.getBody(req);
+      console.log(body);
+      const json = JSON.parse(body);
+      const command =  "command" in json ? json.command : json;
+      await handler(new HttpMessage(command, res));
+    }).listen(this.port);
+    console.log(`Listening on port ${this.port}`);
   }
 
   async disconnect() {
-    if (this.keepaliver) {
-      await this.keepaliver.close();
-    }
     if (this.server) {
       this.server.close();
     }
@@ -125,9 +98,9 @@ class PubSubMessage implements CommandMessage {
     this.pubsub = pubsub;
     this.screenshotsTopic = screenshotsTopic;
   }
-  async publishScreenshot(screenshot: string, sessionID: string): Promise<void> {
+  async publishScreenshot(screenshot: string, sessionID: string, url: string): Promise<void> {
     console.log("publishing screenshot for", this.id, "to topic", this.screenshotsTopic);
-    const json = {id: this.id, session_id: sessionID, screenshot};
+    const json = {id: this.id, session_id: sessionID, screenshot, url};
     await this.pubsub.topic(this.screenshotsTopic).publishMessage({json});
   }
 
@@ -155,19 +128,24 @@ export class PubSubChannel implements MessagingChannel {
     this.interval = null
   }
 
-  async subscribe(handler: (message: CommandMessage) => Promise<void>) {
+  subscribe(handler: (message: CommandMessage) => void) {
     const topic = this.pubsub.topic(this.commandsTopic);
     console.log(`Creating Subscription ${this.subscriptionName} to topic: ${this.commandsTopic}`);
-    const [subscription] = await topic.createSubscription(this.subscriptionName);
-    this.subscription = subscription
-    console.log(`Subscription ${subscription.name} created`);
+    topic.createSubscription(this.subscriptionName, (error, subscription) => {
+      if (error || !subscription) {
+        throw "Failed to create subscription";
+      }
+      this.subscription = subscription;
+      console.log(`Subscription ${subscription.name} created`);
 
-    subscription.on('message', (msg: Message) => {
-        msg.ack();
-        const data = JSON.parse(msg.data.toString());
-        const c = new PubSubMessage(data.command, data.id, this.pubsub, this.screenshotsTopic);
-        handler(c);
+      subscription.on('message', (msg: Message) => {
+          msg.ack();
+          const data = JSON.parse(msg.data.toString());
+          const c = new PubSubMessage(data.command, data.id, this.pubsub, this.screenshotsTopic);
+          handler(c);
+      });
     });
+    
 
     //hack to keep the process running
     this.interval = setInterval(() => {}, 1 << 30);
@@ -179,68 +157,6 @@ export class PubSubChannel implements MessagingChannel {
     }
     if(this.interval){
         clearInterval(this.interval);
-    }
-  }
-}
-
-class RedisMessage implements CommandMessage {
-  id: string;
-  command: string|Record<string, any>;
-  client: RedisClient;
-
-  constructor(command: string, id: string, client: RedisClient) {
-    this.id = id;
-    this.command = command;
-    this.client = client;
-  }
-  async publishScreenshot(screenshot: string, sessionID: string): Promise<void> {
-    console.log("sending screenshot for", this.id)
-    await this.client.publish("screenshots", JSON.stringify({ id: this.id, session_id: sessionID, screenshot }));
-    console.log("screenshot submitted for", this.id)
-  }
-
-  async publishError(error: string): Promise<void> {
-    console.log("publishing error for: ", this.id);
-    const data = {id: this.id, error};
-    await this.client.publish("screenshots", JSON.stringify(data));
-    console.log("error submitted for", this.id);
-  }
-}
-
-export class RedisChannel implements MessagingChannel {
-  private client?: RedisClient;
-  private subscriber?: RedisClient;
-
-  constructor(
-    private redisHost: string,
-    private redisPort: string,
-    private sessionID: string,
-  ){}
-
-  async subscribe(handler: (message: CommandMessage) => Promise<void>) {
-    this.client = await createClient({
-      url: `redis://${this.redisHost}:${this.redisPort}`,
-    })
-      .on('error', err => console.log('Redis Client Error', err))
-      .connect();
-
-    this.subscriber = this.client.duplicate();
-    this.subscriber.on('error', err => console.error(err));
-
-    await this.subscriber.connect();
-    await this.subscriber.subscribe(`session:${this.sessionID}`, async(data: string) => {
-      const eventData = JSON.parse(data);
-      const message = new RedisMessage(eventData.command, eventData.id, this.client as RedisClient);
-      await handler(message);
-    });
-  }
-
-  async disconnect() {
-    if (this.subscriber) {
-      await this.subscriber.disconnect();
-    }
-    if (this.client) {
-      await this.client.disconnect();
     }
   }
 }
